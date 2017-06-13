b793c6c09a856168f3192d073aa179c214ba5b3e
Olga Zhizhka <helgaj@yandex.ru>
2017-06-13 22:40:25 +0300

    Revert "Homework11"

--- homework11/src/app/filter-films-service.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { FILMS } from './filter-films';
-
-@Injectable()
-export class FilterFilmsServiceService {
-  getFilteredFilms(): Promise<string[]> {
-    return Promise.resolve(FILMS);
-  }
-}
